6c7e9680648578134fea8473c544b703fe6ddc38
Harshvardhan Thanekar <139256502+HarshvardhanT42@users.noreply.github.com>
2024-10-26 20:02:07 +0530

    Update SWGusinginteger.py

--- SWGusinginteger.py
+++ SWGusinginteger.py
@@ -1,36 +1,41 @@
+
 import random
 
 def check(comp, user):
-  if comp ==user:
-    return 0
-    
-  if(comp == 0 and user ==1):
-    return -1
-    
-  if(comp == 1 and user ==2):
-    return -1
-    
-  if(comp == 2 and user == 0):
-    return -1
+    if comp == user:
+        return 0
+    if(comp == "Snake" and user == "Water"):
+        return -1
+    if(comp == "Water" and user == "Gun"):
+        return -1
+    if(comp == "Gun" and user == "Snake"):
+        return -1
 
-  return 1
-    
-  
-comp = random.randint(0, 2)
-user = int(input("0 for Snake, 1 for water and 2 for Gun:\n"))
+    return 1
 
-score = check(comp, user)
+foo = ["Snake","Gun","Water"]
+comp = random.choice(foo)
 
-print("You: ", user)
-print("Computer: ", comp)
+user_choise= input("\n1 Sanke \n 2 Water \n 3 Gun\n Enter Your Choise")
 
-if(score == 0):
-  print("Its a draw")
-elif (score == -1):
-  print("You Lose")
+if user_choise == "1":
+    user = "Snake"
+elif user_choise == "2":
+    user = "Water"
+elif user_choise == "3":
+    user = "Gun"
 else:
-  print("You Won")
-  
+    print("Invalid Input")
+    
+    
+if user:
+    score = check(comp, user)
+    print("you: ",user)
+    print("computer: ",comp)
 
-
-
+    if(score == 0):
+        print("its draw")
+    elif(score == -1):
+        print("you lose")
+    else:
+        print("you won")
